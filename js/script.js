d6fbd9e7601382c7d7cca862f54e019715dae42a
FedericoCattani <federico.cattani1@gmail.com>
2023-03-24 17:20:44 +0100

    event click for loop counter

--- js/script.js
+++ js/script.js
@@ -7,10 +7,34 @@ const images = [
   "img/05.webp",
 ]
 
+const itemsContainer = document.querySelector('.items-container');
+const btnUp = document.querySelector('.up');
+const btnDown = document.querySelector('.down');
+
+const items = document.getElementsByClassName('item');
+items[counterImage].classList.remove('item');
+
+
+let counterImage = 0;
+
 
 //LOGIC
+for (i = 0; i < images.length; i++){
 
+  const image = images[i];
 
+  items.innerHTML +=`
+  <img class= 'item' src="${image}">
+  `
+}
 
+btnUp.addEventListener('click', function(){
+  items[counterImage].classList.add('hide');
+
+  counterImage++;
+
+  items[counterImage].classList.remove('hide');
+
+})
 
 //RESULTS
